ce2bba2a4ffce3fec78c70ac395efaa4333c1b47
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2025-02-02 18:51:53 +0300

    Clear remote repository

--- Casa-Connect/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Casa_Connect.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
