c27a7cb1a2991bd07a628a805f8690b428646f59
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2025-11-04 01:13:07 +0300

    Updated core module

--- CasaConnect/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CasaConnect.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
